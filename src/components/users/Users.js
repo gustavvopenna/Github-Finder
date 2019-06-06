7a15e6e1ed264f912458e369e1ffa82bfffbbe24
gustavvopenna <gustavo.pema@gmail.com>
2019-06-06 17:28:55 -0500

    Add loading animation while data is fetch

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,39 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UsersItem from './UsersItem'
+import Spinner from '../layout/Spinner'
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 1,
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-      {
-        id: 2,
-        login: 'defunkt',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt'
-      },
-      {
-        id: 3,
-        login: 'pjhyett',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett'
-      }
-    ]
-  }
-  render() {
-    const { users } = this.state
-    return (
-      <div style={userStyle}>
-        {users.map(user => (
-          <UsersItem key={user.id} user={user} />
-        ))}
-      </div>
-    )
-  }
+const Users = ({ users, loading }) => {
+  return loading ? (
+    <Spinner />
+  ) : (
+    <div style={userStyle}>
+      {users.map(user => (
+        <UsersItem key={user.id} user={user} />
+      ))}
+    </div>
+  )
 }
 
 const userStyle = {
