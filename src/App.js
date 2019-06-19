5bc5f0f1ada70f8598ced80dc1ff6c6271384b10
gustavvopenna <gustavo.pema@gmail.com>
2019-06-18 23:55:55 -0500

    Create context for alert

--- src/App.js
+++ src/App.js
@@ -9,40 +9,37 @@ import About from './components/pages/About'
 import User from './components/users/User'
 
 import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
 
 const App = () => {
   const [alert, setAlert] = useState(null)
 
-  //Set Alert when user does not enter somethig in the search input
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type })
-    setTimeout(() => setAlert(null), 3000)
-  }
-
   return (
     <GithubState>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <div className="container">
-            <Alert alert={alert} />
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={props => (
-                  <Fragment>
-                    <Search setAlert={showAlert} />
-                    <Users />
-                  </Fragment>
-                )}
-              />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/:login" component={User} />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={props => (
+                    <Fragment>
+                      <Search />
+                      <Users />
+                    </Fragment>
+                  )}
+                />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/:login" component={User} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   )
 }
